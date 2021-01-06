0ae1bbf536a0badd259797844d013a4fdfa54d98
asyncfoonctions <66491573+asyncfoonctions@users.noreply.github.com>
2021-01-06 12:42:18 -0500

    Update config.py

--- core/config.py
+++ core/config.py
@@ -29,7 +29,7 @@ class ConfigManager:
         "main_category_id": None,
         "fallback_category_id": None,
         "prefix": "?",
-        "mention": "@here",
+        "mention": "",
         "main_color": str(discord.Color.blurple()),
         "error_color": str(discord.Color.red()),
         "user_typing": False,
